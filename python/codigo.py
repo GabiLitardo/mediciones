50cb1b03de76c24ec7aaa667c3522ae2461465ba
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-06-23 12:48:04 -0300

    Update codigo.py

--- python/codigo.py
+++ python/codigo.py
@@ -3,19 +3,20 @@ import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 
-directorio_base = Path(".")
-# Buscamos y guardamos la lista de rutas en 'lista_de_rutas'
-lista_de_rutas = directorio_base.glob(
-    "**/MOSISV72M_DIE4_PFGIW1_VG=0_postrad*_M1_2.ri"
-)
+def matchear_archivos (nombre_archivo_generico):
+    directorio_base = Path(".")
+    # Buscamos y guardamos la lista de rutas en 'lista_de_rutas'
+    lista_de_rutas = directorio_base.glob(
+        "**/"+nombre_archivo_generico
+    )
+    mediciones = []
+    for ruta_archivo in lista_de_rutas:
+        medicion = np.genfromtxt(ruta_archivo, skip_header=2, usecols=(0, 1), encoding="cp1252")
+        mediciones.append(medicion)
+    return mediciones
 
-super_arreglo = []
 
-# Recorremos la lista de rutas explícitamente
-for ruta_archivo in lista_de_rutas:
-    datos = np.genfromtxt(ruta_archivo, skip_header=2, usecols=(0, 1), encoding="cp1252")
-    super_arreglo.append(datos)
-
+matchear_archivos("MOSISV72M_DIE4_PFGIW1_VG=0_postrad*_M1_2.ri")
 st.write(f"¡Hecho! Se cargaron {len(super_arreglo)} mediciones.")
 st.write(f"{len(super_arreglo)}")
 
