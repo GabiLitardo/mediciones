c5c0d18291c50200439337483d6ce07ce6057c7b
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-06-24 09:54:46 -0300

    Create procesamiento.py

---
+++ python/procesamiento.py
@@ -0,0 +1,12 @@
+# procesamiento.py
+from pathlib import Path
+import numpy as np
+
+def matchear_archivos(nombre_archivo_generico):
+    directorio_base = Path(".")
+    lista_de_rutas = directorio_base.glob("**/" + nombre_archivo_generico)
+    mediciones = []
+    for ruta_archivo in lista_de_rutas:
+        medicion = np.genfromtxt(ruta_archivo, skip_header=2, usecols=(0, 1), encoding="cp1252")
+        mediciones.append(medicion)
+    return mediciones
