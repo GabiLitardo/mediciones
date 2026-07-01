93a90c968f723b0229a5d77f4d2892d0401f4d92
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-01 17:34:27 -0300

    Update graficos.py

--- python/graficos.py
+++ python/graficos.py
@@ -35,30 +35,29 @@ def graficar_dispositivos(titulo, ylabel, datos_procesados, tanda, es_fg):
     plt.close(fig_mpl)
     
 def graficar_sensibilidad_fg(titulo, datos_sensibilidad, xlabel, ylabel):
-    """Dibuja la sensibilidad."""
+    """Dibuja la sensibilidad normalizada"""
     fig_mpl = plt.figure(figsize=(10, 5))
     fig_ply = go.Figure()
-    '''
-    datos_sensibilidad_analitico = datos_sensibilidad["analitico"]
-    for disp, datos in datos_sensibilidad_analitico.items():
-        plt.plot(datos["x"], datos["y"], "o--", label=disp)
-        fig_ply.add_trace(go.Scatter(x=datos["x"], y=datos["y"], mode='lines+markers', name=disp))
-    '''
-    datos_sensibilidad_discreto = datos_sensibilidad[1]
-    st.write(len(datos_sensibilidad))
+    datos_sensbilidad_continuo = datos_sensibilidad[0]
+    for disp, datos in datos_sensibilidad_continuo.items():
+        plt.plot(datos["x"], datos["y"], "-", label=f"{disp} (Poly Fit g4)")
+        fig_ply.add_trace(go.Scatter(x=datos["x"], y=datos["y"], mode='lines', name=f"{disp} (Poly)"))
+
+    datos_sensbilidad_discreto = datos_sensibilidad[1]
     for disp, datos in datos_sensibilidad_discreto.items():
         plt.plot(datos["x"], datos["y"], "o--", label=disp)
         fig_ply.add_trace(go.Scatter(x=datos["x"], y=datos["y"], mode='lines+markers', name=disp))
-    
+        
     plt.title(titulo)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid(True, linestyle=":", alpha=0.6)
     plt.legend()
     st.pyplot(fig_mpl)
-
-    fig_ply.update_layout(title=titulo, xaxis_title=xlabel, yaxis_title=ylabel, template="plotly_white")
+    
+    fig_ply.update_layout(title=titulo, xaxis_title=r"Corriente Promedio Normalizada I_D_norm [$\mu$A]", yaxis_title=r"Tasa de Cambio normalizada[($\mu$A)/min]", template="plotly_white")
 
     st.plotly_chart(fig_ply, width='stretch')
     plt.close(fig_mpl)
+
 
