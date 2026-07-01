630d290c034c2c6e7ec5416cae2005dd7d8a0e34
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-01 20:01:16 -0300

    Update proc_ruido.py

--- python/proc_ruido.py
+++ python/proc_ruido.py
@@ -11,7 +11,6 @@ def convertir_r_a_temp_steinhart(resistencia):
     return (1.0 / (A_SH + B_SH * ln_R + C_SH * (ln_R ** 3))) - 273.15
 
 def calcular_desvio_archivo(nombre_archivo):
-    """Remueve la deriva térmica lineal del archivo de ruido y extrae el desvío AC neto."""
     lista_mediciones = matchear_archivos(nombre_archivo, tipo_medicion="ruido")
     datos = lista_mediciones[0]
     corriente_uA = np.abs(datos[:, 1]) * 1e6
@@ -23,15 +22,20 @@ def calcular_desvio_archivo(nombre_archivo):
     
     corriente_ruido_uA = corriente_uA - corriente_tendencia
         
-    return np.std(corriente_ruido_uA, ddof=1) * 1000.0 # multiplico por 1000 para que quede en nA
+    return np.std(corriente_ruido_uA, ddof=1) * 1000.0
 
-def calcular_desvio(lista_dispositivos, corrientes):
-    resultados = {}
+def procesar_ruido_tanda(lista_dispositivos, corrientes_nominales):
+    resultado = {}
     for disp in lista_dispositivos:
-        resultados[disp] = {}
-        for corr in corrientes:
+        eje_x = []
+        eje_y = []
+        for corr in corrientes_nominales:
             nombre_archivo = f"MOSISV72M_DIE4_{disp}_VD=-4.5_RUIDO_{corr}u_M1.txt"
             sigma = calcular_desvio_archivo(nombre_archivo)
-            resultados[disp][f"{corr} uA"] = f"{sigma:.2f} nA"
+            
+            eje_x.append(float(corr))
+            eje_y.append(sigma)
+            
+        resultado[disp] = {"x": np.array(eje_x), "y": np.array(eje_y)}
 
-    return resultados
+    return resultado
