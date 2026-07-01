e303bfbbcf965e7b65897416f333445a520249d4
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-01 09:43:36 -0300

    Update lector_archivos.py

--- python/lector_archivos.py
+++ python/lector_archivos.py
@@ -4,22 +4,18 @@ from pathlib import Path
 import numpy as np
 import streamlit as st
 
-def matchear_archivos_iv(nombre_archivo_generico):
-    """Busca un archivo de curvas I-V recursivamente y devuelve su matriz de datos."""
+def matchear_archivos(nombre_archivo_generico, tipo_medicion="iv"):
+    """
+    Busca archivos recursivamente y devuelve una lista con sus matrices de datos.
+    Acepta tipo_medicion="iv" o tipo_medicion="ruido".
+    """
     directorio_base = Path(".")
     lista_de_rutas = directorio_base.glob("**/" + nombre_archivo_generico)
     mediciones = []
     for ruta_archivo in lista_de_rutas:
-        medicion = np.genfromtxt(ruta_archivo, skip_header=2, usecols=(0, 1), encoding="cp1252")
-        mediciones.append(medicion)
-    return mediciones
-
-def matchear_archivos_ruido(nombre_archivo_generico):
-    """Busca archivos de ruido .txt recursivamente y devuelve una lista con sus matrices de datos."""
-    directorio_base = Path(".")
-    lista_de_rutas = directorio_base.glob("**/" + nombre_archivo_generico)
-    mediciones = []
-    for ruta_archivo in lista_de_rutas:
-        medicion = np.genfromtxt(ruta_archivo, delimiter='\t', skip_header=5, usecols=(0, 1, 2), encoding="cp1252")
-        mediciones.append(medicion)
+        if tipo_medicion == "ruido":
+            medicion = np.genfromtxt(ruta_archivo, delimiter='\t', skip_header=5, usecols=(0, 1, 2), encoding="cp1252")
+        else:
+            medicion = np.genfromtxt(ruta_archivo, skip_header=2, usecols=(0, 1), encoding="cp1252")
+        mediciones.append(medicion)       
     return mediciones
