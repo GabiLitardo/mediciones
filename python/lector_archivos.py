88380d7a67fd9646d106ab2bd6962f7ca2750417
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-01 09:34:34 -0300

    Update lector_archivos.py

--- python/lector_archivos.py
+++ python/lector_archivos.py
@@ -20,6 +20,6 @@ def matchear_archivos_ruido(nombre_archivo_generico):
     lista_de_rutas = directorio_base.glob("**/" + nombre_archivo_generico)
     mediciones = []
     for ruta_archivo in lista_de_rutas:
-        medicion = np.genfromtxt(ruta_archivo, skip_header=2, usecols=(0, 1, 2), encoding="cp1252")
+        medicion = np.genfromtxt(ruta_archivo, delimiter='\t', skip_header=5, usecols=(0, 1, 2), encoding="cp1252")
         mediciones.append(medicion)
     return mediciones
