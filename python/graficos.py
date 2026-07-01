6aec8e81fe0d151bcc1aeba26225b98de179ca32
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-01 20:43:33 -0300

    Update graficos.py

--- python/graficos.py
+++ python/graficos.py
@@ -125,7 +125,7 @@ def graficar_superposicion_sens_ruido(titulo, datos_sensibilidad, datos_ruido):
     fig_ply.update_layout(
         title=titulo,
         xaxis=dict(
-            title="Corriente Normalizada $I_{D_norm}$ [$\mu$A]",
+            title=r"Corriente Normalizada $I_{D_norm}$ [$\mu$A]",
             domain=[0, 0.82]
         ),
         yaxis=dict(
