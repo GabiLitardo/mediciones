915eb0b1c83472f12357e33713759be99028f4dc
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-01 16:56:31 -0300

    Update graficos.py

--- python/graficos.py
+++ python/graficos.py
@@ -34,28 +34,3 @@ def graficar_dispositivos(titulo, ylabel, datos_procesados, tanda, es_fg):
     st.plotly_chart(fig_ply, width='stretch')
     plt.close(fig_mpl)
 
-def graficar_sensibilidad_fg(titulo, datos_sensibilidad, xlabel, ylabel):
-    """Dibuja la sensibilidad."""
-    fig_mpl = plt.figure(figsize=(10, 5))
-    fig_ply = go.Figure()
-    
-    datos_sensibilidad_analitico = datos_sensibilidad[0]
-    for disp, datos in datos_sensibilidad_analitico.items():
-        plt.plot(datos["x"], datos["y"], "o--", label=disp)
-        fig_ply.add_trace(go.Scatter(x=datos["x"], y=datos["y"], mode='lines+markers', name=disp))
-
-    datos_sensibilidad_discreto = datos_sensibilidad[1]
-    for disp, datos in datos_sensibilidad_discreto.items():
-        plt.plot(datos["x"], datos["y"], "o--", label=disp)
-        fig_ply.add_trace(go.Scatter(x=datos["x"], y=datos["y"], mode='lines+markers', name=disp))
-    plt.title(titulo)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.grid(True, linestyle=":", alpha=0.6)
-    plt.legend()
-    st.pyplot(fig_mpl)
-
-    fig_ply.update_layout(title=titulo, xaxis_title=xlabel, yaxis_title=ylabel, template="plotly_white")
-
-    st.plotly_chart(fig_ply, width='stretch')
-    plt.close(fig_mpl)
