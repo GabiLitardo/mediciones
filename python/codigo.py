d459cdf1517e09e98b0f29ae2bd00cf2c6e99ac3
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-03 15:53:15 -0300

    Update codigo.py

--- python/codigo.py
+++ python/codigo.py
@@ -100,7 +100,7 @@ if mostrar_ruido:
 
     evos = proc_ruido.obtener_evolucion_ruido(["PFGIW1", "PFGIW2", "PFGIP2"], [100, 150, 200, 250, 350])
     
-    graficos.graficar_evolucion_rudo(
+    graficos.graficar_evolucion_ruido(
         titulo="Señal de Ruido Neto en el Tiempo @ 150 uA",
         todas_las_evos=evos,
         corrientes_a_graficar=[100, 150, 200, 250, 350]
