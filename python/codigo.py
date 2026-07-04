183be1eb448360983550d097addd5ecf8f4c6ef5
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-04 12:57:49 -0300

    Update codigo.py

--- python/codigo.py
+++ python/codigo.py
@@ -103,7 +103,7 @@ if mostrar_ruido:
     graficos.graficar_evolucion_ruido(
         titulo="Corriente vs tiempo (log)",
         todas_las_evos=evos,
-        corrientes_a_graficar=[200] 
+        corrientes_a_graficar=[250] 
     )
 # =====================================================================
 # SECCIÓN 4: RESUMEN
