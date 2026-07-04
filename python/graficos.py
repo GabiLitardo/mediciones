dc6f83dd7d32f68a02947605828c9a448e88e9cd
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-04 11:28:29 -0300

    Update graficos.py

--- python/graficos.py
+++ python/graficos.py
@@ -166,7 +166,7 @@ def graficar_evolucion_ruido(titulo, todas_las_evos, corrientes_a_graficar):
                 
                 plt.plot(x_data, y_data, alpha=0.7, label=f"{disp} @ {corr} uA")
                 
-    plt.xscale('log')
+    #plt.xscale('log')
     plt.title(titulo)
     plt.xlabel("Tiempo [s] (Escala Log)")
     plt.ylabel("Corriente de Ruido Neto [$\mu$A]")
