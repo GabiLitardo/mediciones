e075646b8048fda5cea48cac88aafb53fe0ab440
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-07-04 12:06:03 -0300

    Update graficos.py

--- python/graficos.py
+++ python/graficos.py
@@ -195,7 +195,7 @@ def graficar_evolucion_ruido(titulo, todas_las_evos, corrientes_a_graficar):
         title=titulo,
         xaxis=dict(
             title="Tiempo [s]",
-            type="log"
+            #type="log"
         ),
         yaxis=dict(title="Corriente de Ruido Neto [$\mu$A]"),
         template="plotly_white",
