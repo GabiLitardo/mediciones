24bcf6f64d890995f90e9d72d849d122787c1cd2
GabiLitardo <gabi_litardo_02@hotmail.com>
2026-07-28 12:10:24 -0300

    Elimino comentarios

--- python/proc_temp.py
+++ python/proc_temp.py
@@ -13,7 +13,6 @@ def obtener_datos_I_vs_T(lista_dispositivos, corrientes_nominales, lista_tempera
             for temp in lista_temperaturas:
                 archivo_encontrado = None
                 
-                # Buscamos sistemáticamente priorizando la versión de medición más alta
                 for m_ver in ["M10", "M9", "M8", "M7", "M6", "M5", "M4", "M3", "M2", "M1"]:
                     nombre_buscar = f"*_UTN_DIE4_{disp}_{corr}uA_{temp}_{m_ver}.csv"
                     lista_datos = matchear_archivos(nombre_buscar, tipo_medicion="temperatura")
@@ -29,28 +28,24 @@ def obtener_datos_I_vs_T(lista_dispositivos, corrientes_nominales, lista_tempera
                     v_drain = archivo_encontrado[:, 0]
                     i_drain = archivo_encontrado[:, 1]
                     
-                    # Buscamos el índice más cercano a VD = -4.5V
                     idx_vd = np.argmin(np.abs(v_drain - (-4.5)))
                     
-                    # Guardamos el valor absoluto en uA
                     i_en_v5 = np.abs(i_drain[idx_vd]) * 1e6
                     
                     temps_aux.append(float(temp))
                     corrientes_aux.append(i_en_v5)
             
             if temps_aux:
-                # 1. Nos aseguramos de ordenar los vectores de menor a mayor temperatura
                 indices_orden = np.argsort(temps_aux)
                 x_ordenado = np.array(temps_aux)[indices_orden]
                 y_ordenado = np.array(corrientes_aux)[indices_orden]
                 
-                # 2. Calculamos el coeficiente térmico (pendiente de la recta) con los datos ordenados
                 coefs = np.polyfit(x_ordenado, y_ordenado, deg=1)
                 
                 resultado[disp][corr] = {
                     "x": x_ordenado,
                     "y": y_ordenado,
-                    "alpha": coefs[0]  # Coeficiente térmico absoluto (uA/°C)
+                    "alpha": coefs[0]
                 }
                 
     return resultado
