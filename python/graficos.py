de6ebe18efac5fe3192b47bdb441ba678cc95ec8
GabiLitardo <gabi_litardo_02@hotmail.com>
2026-08-04 12:37:31 -0300

    Prueba

--- python/graficos.py
+++ python/graficos.py
@@ -47,7 +47,7 @@ def graficar_dispositivos(titulo, ylabel, datos_procesados, tanda, es_fg):
     )
     transparent_html = f'<div style="background-color: transparent;">{raw_html}</div>'
 
-    st.iframe(transparent_html, height=500)
+    st.iframe(raw_html, height=500)
     
 def graficar_sensibilidad_fg(titulo, datos_sensibilidad, xlabel, ylabel):
     """Dibuja la sensibilidad normalizada"""
