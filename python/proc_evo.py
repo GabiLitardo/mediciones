ebf9dac722220fcd8c51c8311819696eb3823e1f
GabiLitardo <gabi_litardo_02@hotmail.com>
2026-08-12 12:11:23 -0300

    Update

--- python/proc_evo.py
+++ python/proc_evo.py
@@ -3,11 +3,6 @@ import numpy as np
 import streamlit as st
 from lector_archivos import cargar_medicion_tanda, cargar_curva_iv_referencia
 
-COLUMNAS_DISPOSITIVOS = {
-    "PFGIW1": 0, "PFGIW2": 1, "PFGIW3": 2, "PFGIP2": 3,
-    "FFC1": 0, "FFC2": 1, "FFC3": 2, "FFL": 3, "FFS": 4
-}
-
 FACTOR_TENSION = 0.05
 VALOR_CERO_VOLT_TANDA2 = 57.0
 
@@ -38,39 +33,39 @@ def calcular_fit_polinomico(tiempos_list, corrientes_list):
 
 @st.cache_data
 def obtener_datos_crudos_tanda(lista_dispositivos, tipo_tanda, incluir_fit=True):
+    """
+    Retorna la estructura aplanada unificada para mediciones crudas:
+    {"Etiqueta": {"x": array_tiempos, "y": array_valores}}
+    """
     resultado = {}
     limite = 55 if tipo_tanda == "FOXFET" else 31
 
     for disp in lista_dispositivos:
-        if disp not in COLUMNAS_DISPOSITIVOS:
-            continue
-            
-        col_idx = COLUMNAS_DISPOSITIVOS[disp]
         tiempos, valores = [], []
 
         for i in range(0, limite + 1):
-            mediciones = cargar_medicion_tanda(disp, tipo_tanda, i)
-            if mediciones and len(mediciones) > col_idx:
-                matriz = mediciones[col_idx]
-                if matriz is not None and matriz.size > 0:
-                    t_acum = calcular_tiempo_acumulado(i, tipo_tanda)
-                    
-                    if tipo_tanda == "FOXFET":
-                        col_i, col_v = matriz[:, 0], matriz[:, 1]
-                        v_interp = np.interp(0.1, col_i, col_v)
-                        tiempos.append(t_acum)
-                        valores.append(v_interp)
-                    else:
-                        val_uA = np.abs(matriz[0, 1]) * 1e6
-                        tiempos.append(t_acum)
-                        valores.append(val_uA)
+            matriz = cargar_medicion_tanda(disp, tipo_tanda, i)
+            if matriz is not None and matriz.size > 0:
+                t_acum = calcular_tiempo_acumulado(i, tipo_tanda)
+                
+                if tipo_tanda == "FOXFET":
+                    col_i, col_v = matriz[:, 0], matriz[:, 1]
+                    v_interp = np.interp(0.1, col_i, col_v)
+                    tiempos.append(t_acum)
+                    valores.append(v_interp)
+                else:
+                    val_uA = np.abs(matriz[0, 1]) * 1e6
+                    tiempos.append(t_acum)
+                    valores.append(val_uA)
 
         if tiempos:
             t_arr = np.array(tiempos)
             v_arr = np.array(valores)
             
+            # Puntos medidos
             resultado[f"{disp} (Medido)"] = {"x": t_arr, "y": v_arr}
 
+            # Fit polinómico de grado 4 si corresponde
             if incluir_fit and disp in ["PFGIW1", "PFGIW2", "PFGIW3", "PFGIP2"]:
                 coefs = calcular_fit_polinomico(t_arr.tolist(), v_arr.tolist())
                 t_cont = np.linspace(t_arr.min(), t_arr.max(), 200)
@@ -81,6 +76,10 @@ def obtener_datos_crudos_tanda(lista_dispositivos, tipo_tanda, incluir_fit=True)
 
 @st.cache_data
 def obtener_datos_evolucion_vg(lista_dispositivos, tipo_tanda, incluir_fit=True):
+    """
+    Calcula la evolución de V_FG equivalente y retorna la estructura aplanada unificada:
+    {"Etiqueta": {"x": array_tiempos, "y": array_vg}}
+    """
     datos_crudos = obtener_datos_crudos_tanda(lista_dispositivos, tipo_tanda, incluir_fit=False)
     resultado = {}
 
