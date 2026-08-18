4f7e7ba78c14edeacdbd9d0888645edd6cf885be
GabiLitardo <gabi_litardo_02@hotmail.com>
2026-08-18 11:37:25 -0300

    Update

--- python/codigo.py
+++ python/codigo.py
@@ -284,4 +284,4 @@ elif opcion == "Resumen":
     st.text("Para saber si en esa corriente los dispositivos estarían en saturación, me fijo su tensión de Floating Gate equivalente")
     st.text("Las tensiones equivalentes para ZTC son: PFGIW1->1.69V ; PFGIW2->2.10V ; PFGIP2->2.20V")
     st.text("Ahora quiero ver si esas tensiones cumplen Vg>Vds-Vt, con Vds=-4.5V y Vt a estimar")
-    st.text(f"Se estiman los Vt: PFGIW1->{datos_iv_ref["PFGIW1"]["vt"]} ; (PFGIW2, PFGIP2)->{datos_iv_ref["PFGIW2"]["vt"]}")
+    st.text(f"Se estiman los Vt: PFGIW1->{datos_iv_ref["PFGIW1"]["vt"] :.2f} ; (PFGIW2, PFGIP2)->{datos_iv_ref["PFGIW2"]["vt"] :.2f}")
