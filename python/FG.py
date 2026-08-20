52a7554e315c3800f7e809dc7a7f8d8f5d2e9713
GabiLitardo <gabi_litardo_02@hotmail.com>
2026-08-20 09:44:12 -0300

    Update

--- python/FG.py
+++ python/FG.py
@@ -280,11 +280,17 @@ def render_FG ():
             modo='markers'
         )
 
-        st.text("Se estima que las corrientes de ZTC son: PFGIW1->31.2uA ; PFGIW2->17.4uA ; PFGIP2->20.6uA")
+        ztc_iw1 = 30.58
+        ztc_iw2 = 13.96
+        ztc_ip2 = 17.39
+        st.text(f"Se estima que las corrientes de ZTC son: PFGIW1->{ztc_iw1}uA ; PFGIW2->{ztc_iw2}uA ; PFGIP2->{ztc_ip2}uA")
         st.text("Para saber si en esa corriente los dispositivos estarían en saturación, me fijo su tensión de Floating Gate equivalente")
-        st.text("Las tensiones equivalentes para ZTC son: PFGIW1->-1.69V ; PFGIW2->-2.10V ; PFGIP2->-2.20V")
+        vg_iw1 = {proc_evo.obtener_vg_por_corriente("PFGIW1", ztc_iw1)}
+        vg_iw2 = {proc_evo.obtener_vg_por_corriente("PFGIW2", ztc_iw2)}
+        vg_ip2 = {proc_evo.obtener_vg_por_corriente("PFGIP2", ztc_ip2)}
+        st.text(f"Las tensiones equivalentes para ZTC son: PFGIW1->{vg_iw1}V ; PFGIW2->{vg_iw2}V ; PFGIP2->{vg_ip2}V")
         st.text("Ahora quiero ver si esas tensiones cumplen Vfg<Vt y Vds<Vfg-Vt, con Vds=-4.5V y Vt a estimar")
         st.text(f"Se estiman los Vt: PFGIW1->{datos_iv_ref["PFGIW1"]["vt"] :.2f} ; (PFGIW2, PFGIP2)->{datos_iv_ref["PFGIW2"]["vt"] :.2f}")
-        st.text(f"Entonces para el PFGIW1: -1.69V < {datos_iv_ref["PFGIW1"]["vt"] :.2f} y -4.5V < {-1.69 - datos_iv_ref["PFGIW1"]["vt"] :.2f}")
-        st.text(f"Para el PFGIW2: -2.10V < {datos_iv_ref["PFGIW2"]["vt"] :.2f} y -4.5V < {-2.10 - datos_iv_ref["PFGIW2"]["vt"] :.2f}")
-        st.text(f"Para el PFGIP2: -2.20V < {datos_iv_ref["PFGIW2"]["vt"] :.2f} y -4.5V < {-2.20 - datos_iv_ref["PFGIW2"]["vt"] :.2f}")
+        st.text(f"Entonces para el PFGIW1: {vg_iw1}V < {datos_iv_ref["PFGIW1"]["vt"] :.2f} y -4.5V < {-1.69 - datos_iv_ref["PFGIW1"]["vt"] :.2f}")
+        st.text(f"Para el PFGIW2: {vg_iw2}V < {datos_iv_ref["PFGIW2"]["vt"] :.2f} y -4.5V < {-2.10 - datos_iv_ref["PFGIW2"]["vt"] :.2f}")
+        st.text(f"Para el PFGIP2: {vg_ip2}V < {datos_iv_ref["PFGIW2"]["vt"] :.2f} y -4.5V < {-2.20 - datos_iv_ref["PFGIW2"]["vt"] :.2f}")
