97756f9834eb612802f27edd0790b63ddd4c6057
GabiLitardo <gabi_litardo_02@hotmail.com>
2026-08-20 10:16:46 -0300

    Update

--- python/FG.py
+++ python/FG.py
@@ -226,7 +226,7 @@ def render_FG ():
                 )
 
         for disp in ["PFGIW1", "PFGIW2", "PFGIP2"]:
-            st.text(r"$I_{ZTC}$"+f" para {disp}: {datos_temp["alpha_vs_i"][f"{disp} (Fit)"]["ztc"]}")
+            st.text(r"$I_{ZTC}$"+f" para {disp}: {datos_temp["alpha_vs_i"][f"{disp} (Fit)"]["ztc"] :.2f}")
     # =====================================================================
     # SECCIÓN 5: RESUMEN
     # =====================================================================
